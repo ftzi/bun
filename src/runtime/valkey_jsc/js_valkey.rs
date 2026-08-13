86d47bcd1119c2780e577d6e09a2dfc21f1c6623
Alistair Smith <hi@alistair.sh>
2026-08-13 16:33:40 -0700

    valkey: count idle time from connect and restart it on incoming data

--- src/runtime/valkey_jsc/js_valkey.rs
+++ src/runtime/valkey_jsc/js_valkey.rs
@@ -1265,6 +1265,8 @@ impl JSValkeyClient {
         debug_assert!(self.client.get().status == valkey::Status::Connected);
         // we should always have a strong reference to the object here
         debug_assert!(self.this_value.get().is_strong());
+        // Now counting idle time, not connect time.
+        self.reset_connection_timeout();
 
         let self_ptr = self.as_ctx_ptr();
         let _defer = scopeguard::guard(self_ptr, |p| {
@@ -1984,6 +1986,9 @@ impl<const SSL: bool> SocketHandler<SSL> {
 
         let _guard = this.ref_scope();
         let _ = this.client_mut().on_data(data); // TODO: properly propagate exception upwards
+        if this.client.get().status == valkey::Status::Connected {
+            this.reset_connection_timeout();
+        }
         this.update_poll_ref();
     }
 
